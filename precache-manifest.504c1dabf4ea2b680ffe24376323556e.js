178ef87873dac80e355afb876c54832524a2cf0b
PunitLodha <punitlodha@pm.me>
2020-08-19 17:02:42 +0530

    Updates

---
+++ precache-manifest.504c1dabf4ea2b680ffe24376323556e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25fcef57b7e3d733e34f405d29a7a09",
+    "url": "/VisualizationColorPalette/index.html"
+  },
+  {
+    "revision": "50b0999c5ebbdb24d2ee",
+    "url": "/VisualizationColorPalette/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c67024bb861e165cbe9",
+    "url": "/VisualizationColorPalette/static/js/2.d64e8a69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VisualizationColorPalette/static/js/2.d64e8a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b0999c5ebbdb24d2ee",
+    "url": "/VisualizationColorPalette/static/js/main.4d55ca1a.chunk.js"
+  },
+  {
+    "revision": "9e5b759feb8eec4a0a99",
+    "url": "/VisualizationColorPalette/static/js/runtime-main.84da4e41.js"
+  }
+]);
