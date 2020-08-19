ff286de93f54a7c006df71b13f546b7909648cf9
PunitLodha <punitlodha@pm.me>
2020-08-19 17:22:44 +0530

    added padding to charts

--- src/components/GraphsContainer.js
+++ src/components/GraphsContainer.js
@@ -4,6 +4,7 @@ import Graph from './Graph';
 
 const Grid = styled.div`
   display: grid;
+  padding: 32px 20px;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, minmax(0, 300px));
   grid-gap: 10px;
